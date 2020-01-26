872c931c5d7dd1e9a47296dba119393331892cc8
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2020-01-26 23:51:27 +0100

    Project configured with handlebars, handlebars-load, webpack, react and react-dom

---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppCss from './App.scss';
+const App = () => {
+    return <h1> Hello from BSP </h1>;
+};
+console.log('Working');
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+module.exports = (env) => {
+    console.log(env.mode);
+    return {
+        devtool: 'source-map',
+        mode: env.mode,
+        //Enttry point of the app
+        entry: './src/index.js',
+        output: {
+            filename: 'assets/js/bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.hbs$/,
+                    loader: 'handlebars-loader',
+                    options: {
+                        partialDirs: path.join(
+                            __dirname,
+                            'src',
+                            'template',
+                            'partials'
+                        ),
+                    },
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        'style-loader',
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                name: '[name].[contenthash].[ext]',
+                                outputPath: 'assets/img/',
+                                //   publicPath: './assets/',
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'html-loader',
+                            options: { minimize: true },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, './src/template/index.hbs'),
+                title: 'bsp App',
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'assets/css/[name].css',
+                chunkFilename: '[name].css',
+            }),
+        ],
+    };
+};
